16f089b2ffb20a088ecc51388448639c42b6cb81
Shreya Sharma <shreyas@audaxlabs.com>
2023-11-25 20:47:27 +0530

    Added notebook, script and pipeline for training

--- src/ChickenDiseaseClassifier/pipeline/stage_03_training.py
+++ src/ChickenDiseaseClassifier/pipeline/stage_03_training.py
@@ -1,11 +1,19 @@
 from src.ChickenDiseaseClassifier.config.configuration import ConfigurationManager
 from src.ChickenDiseaseClassifier.components.prepare_callbacks import PrepareCallback
+from src.ChickenDiseaseClassifier.components.training import Training
  
+
 try:
     config = ConfigurationManager()
     prepare_callbacks_config = config.get_prepare_callback_config()
     prepare_callbacks = PrepareCallback(config=prepare_callbacks_config)
     callback_list = prepare_callbacks.get_tb_ckpt_callbacks()
+
+    training_config = config.get_training_config()
+    training = Training(config=training_config)
+    training.get_base_model()
+    training.train_valid_generator()
+    training.train(callback_list=callback_list)
 
 except Exception as e:
     raise e
